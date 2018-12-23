cb7390b78d5f1dbc2bf4d0d1b5e1c5a0886b07ca
kazi2018 <rahmanhkazi@gmail.com>
2018-12-23 15:28:45 -0500

    Factorial

--- src/math/problems/Factorial.java
+++ src/math/problems/Factorial.java
@@ -12,5 +12,31 @@ public class Factorial {
          *
          */
 
-    }
-}
+        System.out.println("Factorial of 5 (Recursion): "+factorialRecursive(5));
+        System.out.println("Factorial of 5 (Iteration): "+factorialIterative(5));
+            }
+
+            public static int factorialRecursive(int n){
+                if(n <= 1){
+                    return 1;
+                }
+                else{
+                    return n*factorialRecursive(n-1);
+                }
+            }
+
+            public static int factorialIterative(int n){
+                int result=1,i=1;
+                while(i<=n){
+                    result=result*i;
+                    i++;
+                }
+
+                return result;
+            }
+
+        }
+
+
+
+
