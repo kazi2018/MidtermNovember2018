2f6b7a2fd3602f2072818218ed8c5e38c48cf91f
kazi2018 <rahmanhkazi@gmail.com>
2018-12-23 18:19:50 -0500

    Prime & Pattern answer

--- src/math/problems/PrimeNumber.java
+++ src/math/problems/PrimeNumber.java
@@ -12,7 +12,18 @@ public class PrimeNumber {
 		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
 		 *
 		 */
+		for (int i = 2; i <= 10; i++) {
+			for (int j = 2; j <= i; j++) {
+				if (j == i) {
+					System.out.println(i);
+				}
+				if (i % j == 0) {
+					break;
+				}
+			}
+
+
+		}
 
 	}
-
 }
