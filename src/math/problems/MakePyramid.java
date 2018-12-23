d84234bcd1bf44b0ced54677b37470ef3a2ce39c
kazi2018 <rahmanhkazi@gmail.com>
2018-12-23 13:27:48 -0500

    Pyramid

--- src/math/problems/MakePyramid.java
+++ src/math/problems/MakePyramid.java
@@ -14,6 +14,21 @@ public class MakePyramid {
 
         */
 
+       for(int i=0;i<50;i++) {  // took 50 numbers for the size of the pyramid
+                    for(int j=0;j<50-i;j++) {
+                        System.out.print(" ");
+                    }
 
+                    for (int k=0;k<=i;k++) {
+                        System.out.print("* ");
+                    }
+                    System.out.println();
+                }
+
+
+            }
         }
-  }
+
+
+
+
